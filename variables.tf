7b1c8d11c7490e255a4dfccd975951810189afdd
frengky <frengky@komerce-tech.id>
2024-10-31 16:35:00 +0700

    testing opentofu

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "droplet_name" {
+  description = "The name of the DigitalOcean droplet"
+  type        = string
+  default     = "example-droplet"
+}
+
+variable "region" {
+  description = "The region where the droplet will be created"
+  type        = string
+  default     = "nyc3"
+}
+
+variable "size" {
+  description = "The size of the droplet"
+  type        = string
+  default     = "s-1vcpu-1gb"
+}
+
+variable "image" {
+  description = "The image used for the droplet"
+  type        = string
+  default     = "ubuntu-20-04-x64"
+}
